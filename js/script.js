4fdb8802e26fd1b17514c090f4b2c3c46d477c6b
Khaled Adad <khaledadad@lewisu.edu>
2021-03-24 10:56:18 -0500

    generate cards for multiple users

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-fetch('https://randomuser.me/api/')
+fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    .then(data => generateUser(data))
+    .then(data => generateUsers(data))
 
 // helper functions
-function generateUser(data) {
+
+/** 
+ * Creates user cards
+ * @return {array} returns an array of user cards to be displayed
+ */
+
+function generateUsers(data) {
     // console.log(data)
     const gallery = document.getElementById('gallery');
-    const html = `
-      <div class="card">
-          <div class="card-img-container">
-              <img class="card-img" src="${data.results[0].picture.large}" alt="profile picture">
-          </div>
-          <div class="card-info-container">
-              <h3 id="name" class="card-name cap">${data.results[0].name.first} ${data.results[0].name.last}</h3>
-              <p class="card-text">${data.results[0].email}</p>
-              <p class="card-text cap">${data.results[0].location.city}, ${data.results[0].location.state}</p>
-          </div>
-      </div>
-  `;
+    let html = '';
+    for (let i = 0; i < data.results.length; i++) {
+        html += `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${data.results[i].picture.large}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+                <p class="card-text">${data.results[i].email}</p>
+                <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+            </div>
+        </div>
+    `;
+
+    }
+
     gallery.innerHTML = (html);
 }
