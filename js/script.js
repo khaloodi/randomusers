64ce00bfecd87591a4a46408dbefbac81f0e4a47
Khaled Adad <khaledadad@lewisu.edu>
2021-03-28 10:05:28 -0500

    modal is up

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    .then(data => generateUsers(data))
+    .then(data => {
+        generateUsers(data)
+        addModal(data)
+    })
 
 // helper functions
 
@@ -30,4 +33,29 @@ function generateUsers(data) {
     }
 
     gallery.innerHTML = (html);
+}
+
+/** 
+ * Creates and displays modal
+ */
+
+function addModal() {
+    let modal = `
+            <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">name</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                        <hr>
+                        <p class="modal-text">(555) 555-5555</p>
+                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                        <p class="modal-text">Birthday: 10/21/2015</p>
+                    </div>
+                </div>
+            </div>
+    `;
+    gallery.insertAdjacentHTML('afterend', modal)
 }
